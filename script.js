5d067a02562fb552c070c88c18484ddb51f9b2c6
jaredmasters <mastersjared3@gmail.com>
2025-11-02 03:12:51 -0800

    add: click display for number and clear

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 const buttonsGrid = document.querySelector(".buttons-grid");
+const inputView = document.querySelector(".input-view");
+
+let num1;
+let num2;
+let operator = "";
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+": return add(a, b);
+        case "-": return subtract(a, b);
+        case "*": return multiply(a, b);
+        case "/": return divide(a, b);
+    }
+}
 
 buttonsGrid.addEventListener("mouseover", e => {
     if (e.target.classList.contains("button")) {
@@ -11,3 +41,23 @@ buttonsGrid.addEventListener("mouseout", e => {
         e.target.style.backgroundColor = "rgb(225, 225, 225)";
     }
 });
+
+buttonsGrid.addEventListener("click", e => {
+    if (e.target.classList.contains("button-number")) {
+        if (inputView.textContent == "0") {
+            inputView.textContent = e.target.textContent;
+        } else {
+            inputView.textContent += e.target.textContent;
+        }
+        
+        if (operator == "") {
+            num1 = Number(inputView.textContent);
+        } else {
+            num2 = Number(inputView.textContent);
+        }
+    }
+
+    if (e.target.classList.contains("button-clear")) {
+        inputView.textContent = "0";
+    }
+})
